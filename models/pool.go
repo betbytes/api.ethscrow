95b120c1c1d4588bc2e70427b05f09f0dd917170
Ahmad Ayubi <aayub041@uottawa.ca>
2022-01-15 17:11:13 -0500

    Add better error handling

--- models/pool.go
+++ models/pool.go
@@ -3,6 +3,7 @@ package models
 import (
 	"api.ethscrow/utils/database"
 	"context"
+	"errors"
 )
 
 type Pool struct {
@@ -16,6 +17,9 @@ type Pool struct {
 const poolExists = "SELECT bettor, caller, mediator, reason WHERE id=$1"
 
 func (p *Pool) Exists() (bool, error) {
+	if p.ID == "" {
+		return false, errors.New("missing pool id")
+	}
 	if err := database.DB.QueryRow(context.Background(), poolExists, p.ID).Scan(&p.Bettor, &p.Caller, &p.Mediator, &p.Reason); err != nil {
 		return false, err
 	}
@@ -25,6 +29,9 @@ func (p *Pool) Exists() (bool, error) {
 const createPool = "INSERT INTO pools VALUES ($1, $2, $3, $4, $5)"
 
 func (p *Pool) Create() error {
+	if p.ID == "" || p.Bettor == "" || p.Caller == "" || p.Mediator == "" {
+		return errors.New("missing parameter")
+	}
 	_, err := database.DB.Exec(context.Background(), createPool, p.ID, p.Bettor, p.Caller, p.Mediator, p.Reason)
 	return err
 }
@@ -32,6 +39,9 @@ func (p *Pool) Create() error {
 const closePool = "DELETE FROM pools WHERE id=$1"
 
 func (p *Pool) Close() error {
+	if p.ID == "" {
+		return errors.New("missing pool id")
+	}
 	_, err := database.DB.Exec(context.Background(), closePool, p.ID)
 	return err
 }
--- models/user.go
+++ models/user.go
@@ -3,6 +3,7 @@ package models
 import (
 	"api.ethscrow/utils/database"
 	"context"
+	"errors"
 )
 
 type User struct {
@@ -18,6 +19,9 @@ type Friend struct {
 const existSQL = "SELECT username, public_key FROM users WHERE username=$1)"
 
 func (u *User) Exists() (bool, error) {
+	if u.Username == "" {
+		return false, errors.New("missing username")
+	}
 	if err := database.DB.QueryRow(context.Background(), existSQL, u.Username).Scan(&u.Username, &u.PublicKey); err != nil {
 		return false, err
 	}
@@ -27,6 +31,9 @@ func (u *User) Exists() (bool, error) {
 const registerSQL = "INSERT INTO users VALUES ($1, $2, $3)"
 
 func (u *User) Register() error {
+	if u.Username == "" || u.PublicKey == nil {
+		return errors.New("missing username and/or public key")
+	}
 	_, err := database.DB.Exec(context.Background(), registerSQL, u.Username, u.PublicKey)
 	return err
 }
@@ -34,6 +41,9 @@ func (u *User) Register() error {
 const getFriends = "SELECT u.username, u.public_key FROM friendships f RIGHT JOIN users u on f.friend = u.username WHERE f.user=$1"
 
 func (u *User) GetFriends() ([]Friend, error) {
+	if u.Username == "" {
+		return nil, errors.New("missing username")
+	}
 	rows, err := database.DB.Query(context.Background(), getFriends, u.Username)
 	if err != nil {
 		return nil, err
